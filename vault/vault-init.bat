e1d708e7cbaa4b4757310032d93e2d65215d58ff
Alexandru Smeu <alexandru.smeu@pss.ro>
2021-04-09 12:23:45 +0300

    Small uptates to the script

--- vault/vault-init.bat
+++ vault/vault-init.bat
@@ -1,8 +1,12 @@
 @echo off
 
+pushd %~dp0
+    set SCRIPT_PATH=%CD%
+popd
+
 set VAULT_HOST=localhost
 set VAULT_ADDR=https://%VAULT_HOST%:8200
-set VAULT_CACERT=./certs/localhost.crt
+set VAULT_CACERT=%SCRIPT_PATH%/certs/localhost.crt
 
 rem Init the vault with 1 key and a threshold of 1 (for testing purposes)
 vault operator init -key-shares=1 -key-threshold=1
--- vault/vault-start.bat
+++ vault/vault-start.bat
@@ -1,1 +1,4 @@
-vault server -config ./vault-server-config.hcl
+pushd %~dp0
+    set SCRIPT_PATH=%CD%
+    vault server -config ./vault-server-config.hcl
+popd
